8cd0777543a02f6d34dc7c86c6bb8a697636c6a7
tweniee <abhisheku361@gmail.com>
2025-07-27 23:13:17 +0530

    init

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-const header = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "First H1"),
-    React.createElement("h1", {}, "Second H1"),
+import React from "react";
+import ReactDOM from "react-dom/client";
+const header = React.createElement("div", { key: "1", id: "parent" }, [
+  React.createElement("div", { key: "2", id: "child" }, [
+    React.createElement("h1", {key:"2.2"}, "First H1"),
+    React.createElement("h1", {key:"2.3"}, "Second H1"),
   ]),
-  React.createElement("div",{id:"child2"},[
-    React.createElement("h1",{},"First h1"),
-    React.createElement("h1",{},"Seconf H1")
-  ])
+  React.createElement("div", { key: "3", id: "child2" }, [
+    React.createElement("h1", {key:"3.2"}, "First h1"),
+    React.createElement("h1", {key:"3.3"}, "Seconf H1"),
+  ]),
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(header);
---
+++ dist/Day1.6bd02f5a.js
@@ -0,0 +1,20 @@
+const header = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h1", {}, "First H1"),
+        React.createElement("h1", {}, "Second H1")
+    ]),
+    React.createElement("div", {
+        id: "child2"
+    }, [
+        React.createElement("h1", {}, "First h1"),
+        React.createElement("h1", {}, "Seconf H1")
+    ])
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(header);
+
+//# sourceMappingURL=Day1.6bd02f5a.js.map
